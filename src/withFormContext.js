d5b77197c8ca3d4e6881280037f0705e24856c2c
Bryan Manuele <bryan@flexport.com>
2018-12-17 16:30:22 -0800

    upgraded withFormContext to use AbstractComponent

--- src/withFormContext.js
+++ src/withFormContext.js
@@ -3,45 +3,33 @@
 import * as React from "react";
 import {FormContext, type FormContextPayload} from "./Form";
 
-export default function withFormContext<
-  InnerProps: {+formContext: FormContextPayload},
-  InnerComponent: React.ComponentType<InnerProps>
->(
-  Component: InnerComponent
-): Class<
-  React.Component<
-    $Diff<
-      React.ElementConfig<InnerComponent>,
-      {+formContext: FormContextPayload}
-    >
-  >
-> {
-  class WithFormContext extends React.Component<
-    {forwardedRef: React.Ref<typeof Component>} & $Diff<
-      InnerProps,
-      {formContext: FormContextPayload}
-    >
-  > {
-    render() {
-      const {forwardedRef, ...otherProps} = this.props;
-      return (
-        <FormContext.Consumer>
-          {formContext => (
-            // $FlowFixMe looks right to me
-            <Component
-              {...otherProps}
-              formContext={formContext}
-              // $FlowFixMe I don't understand why you are complaining, Flow
-              ref={forwardedRef}
-            />
-          )}
-        </FormContext.Consumer>
-      );
-    }
-  }
+type InjectedProps = { +formContext: FormContextPayload };
 
-  // $FlowFixMe $FlowBug(0.79.1)
-  return React.forwardRef((props, ref) => {
+export default function withFormContext<InnerProps: InjectedProps, Instance>(
+  Component: React.AbstractComponent<InnerProps, Instance>,
+): React.AbstractComponent<InnerProps, Instance> {
+
+  type WithFormContextProps = {
+    forwardedRef: React.Ref<typeof Component>,
+    ...$Diff<InnerProps, InjectedProps>,
+  };
+
+  const WithFormContext = (
+    {forwardedRef, ...otherProps}: WithFormContextProps,
+  ) => (
+    <FormContext.Consumer>
+      {formContext => (
+        // $FlowFixMe This looks right to me
+        <Component
+          {...otherProps}
+          formContext={formContext}
+          ref={forwardedRef}
+        />
+      )}
+    </FormContext.Consumer>
+  );
+
+  return React.forwardRef<InnerProps, Instance>((props, ref) => {
     return <WithFormContext {...props} forwardedRef={ref} />;
   });
 }
